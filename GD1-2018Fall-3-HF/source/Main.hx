bef6d0de6f8fcd0dc4e096a4b13d68cc321a0770
Jeff Yang <www.speedway@hotmail.com>
2018-09-18 22:32:16 -0400

    uep

---
+++ GD1-2018Fall-3-HF/source/Main.hx
@@ -0,0 +1,14 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+//mport InGameObjects;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(512, 512, MenuState));
+	}
+}
