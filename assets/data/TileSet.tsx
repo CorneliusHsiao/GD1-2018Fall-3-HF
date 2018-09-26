8ef0284a7bf8e4f161d95cb0fbc6a66d171d2206
Kyler Kubo <kubok@rpi.edu>
2018-09-25 20:10:15 -0400

    adding in tileset

---
+++ assets/data/TileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="TileSet" tilewidth="32" tileheight="32" tilecount="18" columns="18">
+ <image source="Git Hub/GD1-2018Fall-3-HF/assets/images/TileSet.png" width="576" height="32"/>
+</tileset>
